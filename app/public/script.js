945c1bf6480f479d70da69efc8f5e773770104bb
akadateppei <62977964+akadateppei@users.noreply.github.com>
2020-05-10 20:58:51 +0900

    httpに変更

--- app/public/script.js
+++ app/public/script.js
@@ -75,72 +75,80 @@ $(function () {
         var others = gameState.others;
 
         //リスナーの処理
-        if(listeners.indexOf(peer.id)!= -1){ //もし自分がリスナーなら
+        if (listeners.indexOf(peer.id) != -1) { //もし自分がリスナーなら
             myState = 'listener';
             listeners.forEach(listener => {
-                if(listener == peer.id){
+                if (listener == peer.id) {
                     console.log("自分はinvisibleVideo");
                     invisibleVideo('myStream');
                     muteVideo('myStream');
-                }else {
+                } else {
                     console.log(listener + "はinvisibleVideo");
                     invisibleVideo(listener);
                     unmuteVideo();
                 }
             });
-        };
-        if(listeners.indexOf(peer.id)== -1) { //もし自分がリスナーでないなら
+        }
+        ;
+        if (listeners.indexOf(peer.id) == -1) { //もし自分がリスナーでないなら
             listeners.forEach(listener => {
                 console.log(listener + "はinvisibleMutedVideo");
                 invisibleVideo(listener);
                 muteVideo(listener);
             });
-        };
+        }
+        ;
 
         // 次の配信者の処理
-        if(nextStreamer == peer.id){
+        if (nextStreamer == peer.id) {
             myState = "nextStreamer";
             console.log("自分はinvisibleMutedVideo");
             invisibleVideo("myStream");
-        }else {
+        } else {
             console.log(nextStreamer + "はinvisibleMutedVideo");
             invisibleVideo(nextStreamer);
             muteVideo(nextStreamer);
-        };
+        }
+        ;
 
         // その他の処理
-        if(others.indexOf(peer.id)!= -1){ //もし自分がotherなら
+        if (others.indexOf(peer.id) != -1) { //もし自分がotherなら
             myState = 'other';
-        };
+        }
+        ;
         others.forEach(other => {
-            if(other == peer.id){
+            if (other == peer.id) {
                 invisibleVideo("myStream");
-            }else {
+            } else {
                 invisibleVideo(other);
                 muteVideo(other);
                 console.log(other + "はinvisibleMutedVideo");
-            };
+            }
+            ;
         });
 
         //今配信してる人の処理
         nowStreamer = streamer;
-        if(myState == "listener"){
-            if(nowStreamer == peer.id){
+        if (myState == "listener") {
+            if (nowStreamer == peer.id) {
                 console.log("自分はvisibleVideo");
                 visibleVideo('myStream');
-            }else {
+            } else {
                 console.log(nowStreamer + "はvisibleVideo");
                 visibleVideo(nowStreamer);
             }
-        };
-        if(myState == "nextStreamer"){
+        }
+        ;
+        if (myState == "nextStreamer") {
             console.log(nowStreamer + "はvisibleMutedVideo");
             visibleVideo(nowStreamer);
-        };
-        if(myState != "listener" && myState != "nextStreamer"){
+        }
+        ;
+        if (myState != "listener" && myState != "nextStreamer") {
             console.log(nowStreamer + "はinvisibleMutedVideo");
             invisibleVideo(nowStreamer);
-        };
+        }
+        ;
     });
 
     function setupGetUserMedia() {
@@ -213,20 +221,20 @@ $(function () {
         $('.videosContainer').append(videoDom);
     }
 
-    function visibleVideo(peerId){
-        $('#'+peerId).show();
+    function visibleVideo(peerId) {
+        $('#' + peerId).show();
     }
 
-    function invisibleVideo(peerId){
-        $('#'+peerId).hide();
+    function invisibleVideo(peerId) {
+        $('#' + peerId).hide();
     }
 
-    function muteVideo(peerId){
-        $('#'+ peerId).attr({'muted': 'true'});
+    function muteVideo(peerId) {
+        $('#' + peerId).attr({'muted': 'true'});
     }
 
-    function unmuteVideo(peerId){
-        $('#'+ peerId).attr({'muted': 'false'});
+    function unmuteVideo(peerId) {
+        $('#' + peerId).attr({'muted': 'false'});
     }
 
     function removeVideo(peerId) {
