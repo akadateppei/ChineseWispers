44980ec1379c52e423a5f1c9a2faa86e8e5c2121
akadateppei <62977964+akadateppei@users.noreply.github.com>
2020-05-10 20:47:29 +0900

    一旦ゲームロジックはできたっぽい？

--- app/public/script.js
+++ app/public/script.js
@@ -1,26 +1,255 @@
-// // const Peer = require('skyway-js');
-// const peer = new Peer({
-//     key: '672ecab7-6a69-4ae8-814f-f3a50e089d6c',
-//     debug: 3
-// });
-//
-// peer.on('open', () => {
-//     document.getElementById('my-id').textContent = peer.id;
-// });
-//
-// let localStream = null;
-//
-// // カメラ映像取得
-// navigator.mediaDevices.getUserMedia({video: true, audio: true})
-//     .then(stream => {
-//         // 成功時にvideo要素にカメラ映像をセットし、再生
-//         const videoElm = document.getElementById('my-video')
-//         videoElm.srcObject = stream;
-//         videoElm.play();
-//         // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
-//         localStream = stream;
-//     }).catch(error => {
-//     // 失敗時にはエラーログを出力
-//     console.error('mediaDevice.getUserMedia() error:', error);
-//     return;
-// });
+$(function () {
+
+    let localStream = null;
+    let peer = null;
+    let existingCall = null;
+    let audioSelect = $('#audioSource');
+    let videoSelect = $('#videoSource');
+    var socket = io();
+    myState = "";
+    nowStreamer = "";
+
+    navigator.mediaDevices.enumerateDevices()
+        .then(function (deviceInfos) {
+            for (let i = 0; i !== deviceInfos.length; ++i) {
+                let deviceInfo = deviceInfos[i];
+                let option = $('<option>');
+                option.val(deviceInfo.deviceId);
+                if (deviceInfo.kind === 'audioinput') {
+                    option.text(deviceInfo.label);
+                    audioSelect.append(option);
+                } else if (deviceInfo.kind === 'videoinput') {
+                    option.text(deviceInfo.label);
+                    videoSelect.append(option);
+                }
+            }
+            videoSelect.on('change', setupGetUserMedia);
+            audioSelect.on('change', setupGetUserMedia);
+            setupGetUserMedia();
+        }).catch(function (error) {
+        console.error('mediaDevices.enumerateDevices() error:', error);
+        return;
+    });
+
+    peer = new Peer({
+        key: '672ecab7-6a69-4ae8-814f-f3a50e089d6c',
+        debug: 3
+    });
+
+    peer.on('open', function () {
+        $('#my-id').text(peer.id);
+    });
+
+    peer.on('error', function (err) {
+        alert(err.message);
+    });
+
+    $('#make-call').submit(function (e) {
+        e.preventDefault();
+        let roomName = $('#join-room').val();
+        if (!roomName) {
+            return;
+        }
+        const call = peer.joinRoom(roomName, {mode: 'sfu', stream: localStream});
+        setupCallEventHandlers(call);
+        // socketでゲーム参加通知
+        socket.emit('login', peer.id);
+        console.log('ログインはした')
+    });
+
+    $('#end-call').click(function () {
+        existingCall.close();
+    });
+
+    $('#game-start').click(function () {
+        socket.emit('game start', '');
+        console.log('ゲームスタート押した');
+    });
+
+    socket.on('game started', (gameState) => {
+        setupStartUI();
+        console.log('ゲームスタート');
+        var listeners = gameState.listeners;
+        var streamer = gameState.streamer;
+        var nextStreamer = gameState.nextStreamer;
+        var others = gameState.others;
+
+        //リスナーの処理
+        if(listeners.indexOf(peer.id)!= -1){ //もし自分がリスナーなら
+            myState = 'listener';
+            listeners.forEach(listener => {
+                if(listener == peer.id){
+                    console.log("自分はinvisibleVideo");
+                    invisibleVideo('myStream');
+                    muteVideo('myStream');
+                }else {
+                    console.log(listener + "はinvisibleVideo");
+                    invisibleVideo(listener);
+                    unmuteVideo();
+                }
+            });
+        };
+        if(listeners.indexOf(peer.id)== -1) { //もし自分がリスナーでないなら
+            listeners.forEach(listener => {
+                console.log(listener + "はinvisibleMutedVideo");
+                invisibleVideo(listener);
+                muteVideo(listener);
+            });
+        };
+
+        // 次の配信者の処理
+        if(nextStreamer == peer.id){
+            myState = "nextStreamer";
+            console.log("自分はinvisibleMutedVideo");
+            invisibleVideo("myStream");
+        }else {
+            console.log(nextStreamer + "はinvisibleMutedVideo");
+            invisibleVideo(nextStreamer);
+            muteVideo(nextStreamer);
+        };
+
+        // その他の処理
+        if(others.indexOf(peer.id)!= -1){ //もし自分がotherなら
+            myState = 'other';
+        };
+        others.forEach(other => {
+            if(other == peer.id){
+                invisibleVideo("myStream");
+            }else {
+                invisibleVideo(other);
+                muteVideo(other);
+                console.log(other + "はinvisibleMutedVideo");
+            };
+        });
+
+        //今配信してる人の処理
+        nowStreamer = streamer;
+        if(myState == "listener"){
+            if(nowStreamer == peer.id){
+                console.log("自分はvisibleVideo");
+                visibleVideo('myStream');
+            }else {
+                console.log(nowStreamer + "はvisibleVideo");
+                visibleVideo(nowStreamer);
+            }
+        };
+        if(myState == "nextStreamer"){
+            console.log(nowStreamer + "はvisibleMutedVideo");
+            visibleVideo(nowStreamer);
+        };
+        if(myState != "listener" && myState != "nextStreamer"){
+            console.log(nowStreamer + "はinvisibleMutedVideo");
+            invisibleVideo(nowStreamer);
+        };
+    });
+
+    function setupGetUserMedia() {
+        let audioSource = $('#audioSource').val();
+        let videoSource = $('#videoSource').val();
+        let constraints = {
+            audio: {deviceId: {exact: audioSource}},
+            video: {deviceId: {exact: videoSource}}
+        };
+        constraints.video.width = {
+            min: 320,
+            max: 320
+        };
+        constraints.video.height = {
+            min: 240,
+            max: 240
+        };
+
+        if (localStream) {
+            localStream = null;
+        }
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(function (stream) {
+                $('#myStream').get(0).srcObject = stream;
+                localStream = stream;
+
+                if (existingCall) {
+                    existingCall.replaceStream(stream);
+                }
+
+            }).catch(function (error) {
+            console.error('mediaDevice.getUserMedia() error:', error);
+            return;
+        });
+    }
+
+    function setupCallEventHandlers(call) {
+        if (existingCall) {
+            existingCall.close();
+        }
+        ;
+
+        existingCall = call;
+        setupEndCallUI();
+        $('#room-id').text(call.name);
+
+        call.on('stream', function (stream) {
+            addVideo(stream);
+        });
+
+        call.on('removeStream', function (stream) {
+            removeVideo(stream.peerId);
+        });
+
+        call.on('peerLeave', function (peerId) {
+            removeVideo(peerId);
+        });
+
+        call.on('close', function () {
+            removeAllRemoteVideos();
+            setupMakeCallUI();
+        });
+    }
+
+    function addVideo(stream) {
+        const videoDom = $('<video autoplay>'); // 本番ではmuted = false
+        videoDom.attr('id', stream.peerId);
+        videoDom.get(0).srcObject = stream;
+        $('.videosContainer').append(videoDom);
+    }
+
+    function visibleVideo(peerId){
+        $('#'+peerId).show();
+    }
+
+    function invisibleVideo(peerId){
+        $('#'+peerId).hide();
+    }
+
+    function muteVideo(peerId){
+        $('#'+ peerId).attr({'muted': 'true'});
+    }
+
+    function unmuteVideo(peerId){
+        $('#'+ peerId).attr({'muted': 'false'});
+    }
+
+    function removeVideo(peerId) {
+        $('#' + peerId).remove();
+    }
+
+    function removeAllRemoteVideos() {
+        $('.videosContainer').empty();
+    }
+
+    function setupMakeCallUI() {
+        $('#make-call').show();
+        $('#end-call').hide();
+    }
+
+    function setupEndCallUI() {
+        $('#make-call').hide();
+        $('#end-call').show();
+    }
+
+    function setupStartUI() {
+        $('#make-call').hide();
+        $('#game-start').hide();
+    }
+
+});
--- app/server.js
+++ app/server.js
@@ -1,17 +1,76 @@
 const express = require('express');
 const app = express();
-
-var http = require('http').createServer(app);
-// var io = require('socket.io')(http);
-
-
-// const Peer = require('skyway-js');
-// const peer = new Peer({key: '672ecab7-6a69-4ae8-814f-f3a50e089d6c'});
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+players = [];
+listeners = [];
+streamer = "";
+nextStreamer = "";
+others = [];
 
 // index.htmlを表示
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    socket.on('login', (id) => {
+        players.push(id);
+        console.log(players);
+    });
+
+    socket.on('game start', () => {
+        listeners = [];
+        streamer = "";
+        nextStreamer = "";
+        others = [];
+        if (players.length > 1){
+            startGame();
+        };
+    });
+
+    function startGame() {
+        console.log("はい");
+        console.log(players.length);
+        for (let i = 0; i !== players.length-1; i++) {
+            listeners.push(players[i]);
+            streamer = players[i];
+            nextStreamer = players[i + 1];
+            others = players.slice(i + 2, players.length + 1);
+            var gameState = {listeners: listeners, streamer: streamer, nextStreamer: nextStreamer, others: others};
+            console.log(gameState);
+            io.emit('game started', gameState);
+            console.log('gameState emitした。');
+            console.log('1秒まつ');
+            wait(40);
+            // wait(31);
+        };
+    };
+
+    socket.on('disconnect', ()=>{
+       console.log('user disconnected') ;
+    });
+});
+
+function getStreamer(players) { // 先頭をplayerとして取り出し、それ以外をlisnersとして配列に入れる。playersの先頭をけつに突っ込む
+    var streamer = players.shift();
+    var listeners = [];
+    players.forEach(player => {
+        listeners.push(player);
+    });
+    players.push(streamer);
+    return listeners;
+};
+
+const wait = (sec) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, sec*1000);
+        //setTimeout(() => {reject(new Error("エラー！"))}, sec*1000);
+    });
+};
+
 
 // port3000でlisten
 http.listen(3000, () => {
